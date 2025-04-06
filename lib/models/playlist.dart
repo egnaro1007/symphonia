7ddd4241c75158a46ea771b7244a2bbf0f874d06
flashhhhh <nguyenchithanhpro196@gmail.com>
2025-04-07 01:58:00 +0700

    Add API to get specific playlist

--- lib/models/playlist.dart
+++ lib/models/playlist.dart
@@ -1,5 +1,7 @@
+import 'package:symphonia/models/song.dart';
+
 class BriefPlayList {
-  String id;
+  int id;
   String title;
   String picture;
   String creator;
@@ -13,8 +15,22 @@ class BriefPlayList {
 }
 
 class PlayList {
-  String? id;
-  String? title;
-  String? description;
-  String? imageUrl;
+  int id;
+  String title;
+  String description;
+  int duration;
+  String picture;
+  String creator;
+
+  List<Song> songs;
+
+  PlayList({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.duration,
+    required this.picture,
+    required this.creator,
+    required this.songs,
+  });
 }
